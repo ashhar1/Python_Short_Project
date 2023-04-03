46e4dc56b829480ffcc0e48513f8892eabbeb34f
Ashhar Ahmed <95365065+ashhar1@users.noreply.github.com>
2023-04-04 00:56:59 +0530

    Create 1_BandNameGenerator.py

---
+++ 1_BandNameGenerator.py
@@ -0,0 +1,10 @@
+#Greeting for program
+print("Welcome to the band name generator")
+
+#Ask user for city name
+city = input("Which city did you grow up in?\n")
+
+#Ask user for pet name
+pet = input("What is the name of your pet?\n")
+
+print("Your band name could be " + city + " " + pet)
